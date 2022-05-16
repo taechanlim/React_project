b62fc190c99173ba14646fa89a0349f8be2356ef
limtaechan <tccap123@naver.com>
2022-05-16 11:20:01 +0900

    디렉토리 구조

---
+++ front/src/Pages/Feeds/Feed_idx.jsx
@@ -0,0 +1,13 @@
+import FeedComment from "./Feed_comment"
+
+function Feed_idx(){
+    return(
+        <>
+        <h1>피드 눌렀을때 view 역할</h1>
+        <h3><FeedComment /></h3>
+        </>
+        
+    )
+}
+
+export default Feed_idx
