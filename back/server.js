32085192cd5a05c2e36d51bd773e15135ebbf1f5
limtaechan <tccap123@naver.com>
2022-05-17 12:17:30 +0900

    커넥션풀 user/register

---
+++ back/server.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const cors = require('cors')
+const cookieParser = require('cookie-parser')
+const router = require('./routes/index.js');
+const app = express()
+
+app.use(express.json())
+app.use(express.urlencoded({extended:true,}))
+// app.use(express.static('./uploads'))
+app.use(cookieParser())
+app.use(cors({
+    origin:true,
+    credentials:true,
+}))
+
+
+app.use(router);
+
+
+
+
+app.listen(4001,()=>{
+    console.log('서버시작 4001')
+})
