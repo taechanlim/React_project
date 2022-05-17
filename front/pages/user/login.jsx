8175b246afa7300c7348097bc63f41ddc4582ffa
limtaechan <tccap123@naver.com>
2022-05-17 16:13:39 +0900

    로그인/로그아웃

--- front/pages/user/login.jsx
+++ front/pages/user/login.jsx
@@ -1,13 +1,15 @@
 import {useState,useEffect} from 'react'
 const axios = require('axios')
+import { useCookies } from 'react-cookie';
 
 
 const Login = () => {
 
-    const [values,setValues] = useState({userid:'',password:''})
+    const [values,setValues] = useState({userid:'',userpw:''})
     const [submit,setSubmit] = useState(false)
     const [isLogin,setisLogin] = useState()
 
+    
     const handleChange = (e) => {
         // console.log(e.target.name,e.target.value)
         const {name,value} = e.target
@@ -16,18 +18,34 @@ const Login = () => {
             [name]:value
         })
     }
+    
+    const COOKIE_KEY = document.cookie
+    const [, , removeCookie] = useCookies([COOKIE_KEY]);
 
-    const logout = e => {
-        setisLogin(false)
+    const logout = async () => {
+        removeCookie(COOKIE_KEY, { path: '/' });
+        location.href='/'
     }
-
-    const handleSubmit = (e) => {
+    const body = {
+        userid:values.userid,
+        userpw:values.userpw
+        
+    }
+    const logincheck = () => {
+        if(document.cookie){
+            setisLogin(true)
+        }
+    }
+    const handleSubmit = async (e) => {
         e.preventDefault()
         setSubmit(true)
+        const result = await axios.post('http://localhost:4001/api/user/login',body,{
+            'Content-type':'application/json',
+            withCredentials:true,
+        })
         
         setTimeout(()=>{
-            const result=true //const [result] = await axios.post('http://localhost:4001/api/user/login)
-            if(result === true){
+            if(result.data.errno === 0){
                 setisLogin(true)
                 alert(`
                     아이디 : ${values.userid}
@@ -41,25 +59,26 @@ const Login = () => {
     }
 
     useEffect(()=>{
-        // console.log('helloworld')
-         
+        logincheck()
     },[isLogin]) //isLogin이 바뀔때마다 코드실행
 
     return(
         <>
              <form onSubmit={handleSubmit}>
             <ul>
+                
+                {isLogin ? <li><button onClick={logout}>로그아웃</button></li> :  <>
                 <li>
-                    <label>이메일</label>
+                    <label>아이디</label>
                     <input type="text" name="userid" onChange={handleChange} />
                 </li>
                 <li>
                     <label>패스워드</label>
-                    <input type="password" name="password" onChange={handleChange} />
+                    <input type="password" name="userpw" onChange={handleChange} />
                 </li>
-                <li>
-                {isLogin ? <button onClick={logout}>로그아웃</button> : <input type="submit" value="로그인" disabled={submit} />}
-                </li>
+                <li><input type="submit" value="로그인" disabled={submit} /></li> 
+                </>}
+                
             </ul>
 
            
