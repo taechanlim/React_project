39ac485f8997c4747482ddd6484b35a6ecb9b9f8
kimhelp <operat8009@gmail.com>
2022-05-17 12:50:45 +0900

    회원가입 완성

--- front/pages/user/register.jsx
+++ front/pages/user/register.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react"
+const axios = require('axios')
 
 const Register = () => {
     const [userid,setUserid] = useState('')
@@ -8,37 +9,43 @@ const Register = () => {
     const [phonenumber,setPhonenumber] = useState('')
     
     const onNameHandler = (event) =>{
-        setUserid(event.currenTarget.value)
+        setUserid(event.target.value)
     }
     const onEmailHandler = (event) =>{
-        setNickname(event.currenTarget.value)
+        setNickname(event.target.value)
     }
     const onPasswordHandler = (event) =>{
-        setUserpw(event.currenTarget.value)
+        setUserpw(event.target.value)
     }
     const onConfirmPasswordHandler = (event)=>{
-        setConfirmPassword(event.currenTarget.value)
+        setConfirmPassword(event.target.value)
     }
-    const onPhoneHandler = ( event)=>{
-        setPhonenumber(event.currenTarget.value)
+    const onPhoneHandler = (event)=>{
+        setPhonenumber(event.target.value)
     }
-    const onSubmit = (event) =>{
+
+    const body = {userid,userpw,nickname,phonenumber}
+
+    const onSubmit = async (event) =>{
         event.preventDefault()
+        const result = await axios.post('http://localhost:4001/api/user/register',body) 
+        console.log(result);
         if(userpw !== confirmPassword){
             return alert('비밀번호와 비밀번호 확인이 불일치')
         }else{
+            
             return alert('성공되다 가입')
         }
     }
     return(
         <div class="loginregister">
-            <form>
+            <form onSubmit={onSubmit}>
                 <div><input name="userid" type="text" placeholder="이름" value={userid} onChange={onNameHandler}/></div>
                 <div><input name="nickname" type="text" placeholder="이메일" value={nickname} onChange={onEmailHandler}/></div>
                 <div><input name="userpw" type="password" placeholder="비밀번호" value={userpw} onChange={onPasswordHandler}/></div>
                 <div><input name="confirmPassword" type="password" placeholder="비밀번호 확인" value={confirmPassword} onChange={onConfirmPasswordHandler}/></div>
                 <div><input name="phonenumber" type="text" placeholder="핸드폰" value={phonenumber} onChange={onPhoneHandler}/></div>
-                <div><button type="submit" onSubmit={onSubmit}>계정 생성하기</button></div>
+                <div><button type="submit">계정 생성하기</button></div>
             </form>
         </div>
     )
