b70089e75b3d45c1a596845ed0f7ee48ee574e3b
limtaechan <tccap123@naver.com>
2022-05-17 10:34:39 +0900

    feed/write

--- front/pages/feed/write.jsx
+++ front/pages/feed/write.jsx
@@ -1,15 +1,46 @@
+import {useState,useEffect} from 'react'
+const axios = require('axios')
 
 const Write = () => {
+
+    const [values,setValues] = useState({userid:'',subject:'',content:''})
+    const [submit,setSubmit] = useState(false)
+    
+    const handleChange = (e) => {
+        console.log(e.target.name,e.target.value)
+        const {name,value} = e.target
+        setValues({
+            ...values,
+            [name]:value
+        })
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        setSubmit(true)
+        
+        setTimeout(()=>{
+            const result=true //const [result] = await axios.post('http://localhost:4001/api/user/login)
+            if(result === true){
+                
+                location.href='/'
+            }else{
+                alert('작성 실패')
+            }
+            setSubmit(false)
+        },1000)
+    }
+
     return(
         <>
             <h1>피드 쓰기</h1>
-            <form>
-                <input type='text' placeholder="제목"></input> <br />
+            <form onSubmit={handleSubmit}>
+                <input type='text' name="subject" placeholder="제목" onChange={handleChange}></input> <br />
                 <input type='file'></input> <br />
                 <input type='file'></input> <br />
                 <input type='file'></input> <br />
-                <input type='text' placeholder="내용"></input> <br />
-                <input type='submit' value='작성'></input>
+                <input type='text' name="content" placeholder="내용" onChange={handleChange}></input> <br />
+                <input type='submit' value='작성' disabled={submit}></input>
             </form>
         </>
     )
