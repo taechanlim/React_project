18f08abe4426b39c9daeeb47fbb6c58432ba4763
kimhelp <operat8009@gmail.com>
2022-05-18 09:30:45 +0900

    ddd

--- back/Database/db.js
+++ back/Database/db.js
@@ -5,7 +5,7 @@ const host = process.env.DB_HOST || 'localhost'
 const user = process.env.DB_USER || 'kimhelp'
 const password = process.env.DB_PASSWORD || 'RLAehdna12@'
 const database = process.env.DB_DATABASE || 'react_pj'
-//kimhelp RLAehdna12@
+
 const config = {host,user,password,database}
 const pool = mysql.createPool(config)
 const promisePool = pool.promise()
