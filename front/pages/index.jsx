a51152db7338c437bb6d3844bf7b354692484d1c
limtaechan <tccap123@naver.com>
2022-05-20 13:39:08 +0900

    dqw

--- front/pages/index.jsx
+++ front/pages/index.jsx
@@ -1,14 +1,73 @@
 import Link from 'next/link'
 import List from './feed/list'
+const axios = require('axios')
+import React, { Fragment } from "react";
 
-const Index = () => {
+
+const Index = (props) => {
+    const list = props.list
+    const deleteBtn = async (i) => {
+        const body = {idx:i}
+        
+        const result = await axios.post('http://localhost:4001/api/feed/delete',body)
+        console.log(result);
+        alert('삭제되었습니다')
+        location.href='/'
+    }
+    const updateBtn = (i) => {
+        console.log(i);
+    }
     return(
         <>
             -------------메인페이지------------ <br />
-            피드 넣어야함
-            <Link href="/feed/list">피드 바로가기</Link>
+            <div>
+            <Fragment>
+                
+                <div>
+                    {list?.map((info) => {
+                    return (
+                    <div key={info.idx}>
+                        {/* <Link href={`/user/main/reservation/detail/${info.idx}`}>
+                        <input type="button" value={"button" + info.idx} alt="" />
+                        </Link> */}
+                        <div>글번호 : {info.idx}</div>
+                        <div />
+                        <div>제목 : {info.subject}</div>
+                        <div />
+                        <div>내용 :{info.content}</div>
+                        <div>작성자 :{info.nickname}</div>
+                        <button onClick={()=>deleteBtn(info.idx)}>삭제</button>
+                        <Link href={`/feed/update?${info.idx}`}><button onClick={()=>updateBtn(info.idx)}>수정</button></Link>
+                        <Link href={`/comment/${info.idx}`}><button>댓글보기</button></Link>
+                        <p>-------------------------------------</p>
+                    </div>
+                    );
+                    })}
+                </div>
+	        </Fragment>
+        </div>
         </>
+        
     )
+
 }
+export async function getServerSideProps(){
+    
+    const response = await axios.get('http://localhost:4001/api/feed/list')
+    
+    const list = response.data.result
+    
+    return{
+        props:{
+            list:list
+        }
+    }
+}
+
+
+
+
+
+
 
 export default Index
