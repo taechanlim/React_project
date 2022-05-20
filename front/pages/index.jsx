13533d6cec0c4a5e34037acebd270c575e6c6528
limtaechan <tccap123@naver.com>
2022-05-20 15:34:14 +0900

    수정삭제 본인만하게 실패

--- front/pages/index.jsx
+++ front/pages/index.jsx
@@ -1,11 +1,35 @@
 import Link from 'next/link'
 import List from './feed/list'
 const axios = require('axios')
-import React, { Fragment } from "react";
+import React, { Fragment,useState,useEffect } from "react";
+
 
 
 const Index = (props) => {
     const list = props.list
+    const [isCookie,setisCookie] = useState()
+    
+    const cookiecheck = () => {
+        if(document.cookie){
+            const token = document.cookie
+            
+            const [,payload,] = token.split('.')
+            const decodingPayload = Buffer.from(payload,'base64').toString()
+            const nickname = JSON.parse(decodingPayload).nickname
+            const matchnickname = list.map(v=>v.nickname)
+            
+            for (let i=0;i<matchnickname.length;i++){
+                // console.log(matchnickname[i])
+                if(matchnickname[i] == nickname){setisCookie(true)}
+            }
+
+        }
+    }
+    useEffect(()=>{
+        cookiecheck()
+        
+    },[isCookie]) 
+
     const deleteBtn = async (i) => {
         const body = {idx:i}
         
@@ -17,6 +41,9 @@ const Index = (props) => {
     const updateBtn = (i) => {
         console.log(i);
     }
+
+    
+
     return(
         <>
             -------------메인페이지------------ <br />
@@ -36,8 +63,12 @@ const Index = (props) => {
                         <div />
                         <div>내용 :{info.content}</div>
                         <div>작성자 :{info.nickname}</div>
-                        <button onClick={()=>deleteBtn(info.idx)}>삭제</button>
-                        <Link href={`/feed/update?${info.idx}`}><button onClick={()=>updateBtn(info.idx)}>수정</button></Link>
+                        {isCookie ? <button onClick={()=>deleteBtn(info.idx)}>삭제</button>
+                                  : <div />        
+                        }
+                        {isCookie ? <Link href={`/feed/update?${info.idx}`}><button onClick={()=>updateBtn(info.idx)}>수정</button></Link>
+                                  : <div />
+                        }
                         <Link href={`/comment/${info.idx}`}><button>댓글보기</button></Link>
                         <p>-------------------------------------</p>
                     </div>
