42dbe210e12320f674f79bcdab9c10877c9c16dc
kimhelp <operat8009@gmail.com>
2022-05-24 17:34:28 +0900

    풀받아.

--- front/components/Footer.jsx
+++ front/components/Footer.jsx
@@ -2,12 +2,14 @@ import {MehOutlined} from '@ant-design/icons'
 const Footer = () => {
     return(
         <footer>
-            <div style={{border:'1px solid lightgray',position:'fixed',bottom:'0',width:'100%',zIndex:'10',background:'#ffffff'}}>
-            <div style={{width:'100%'}}><img src="/static/logo.png" style={{float:'right'}}/></div>
+            <div style={{position:'fixed',bottom:'0',width:'100%',zIndex:'10',backgroundColor:'lightgray'}}>
+            <div style={{width:'100%'}}><img src="/static/logo.png" style={{float:'left'}}/></div>
+                <div style={{marginLeft:'5%'}}>
                 <h2>
                     React_pj
                 </h2>
                 <p style={{fontWeight:'bold'}}>2022-05-16 ~ 2022-05-31 Team : <MehOutlined />임태찬 <MehOutlined />김도움 <MehOutlined />이현진 <MehOutlined />지정환</p>
+                </div>
             </div>
         </footer>
     )
