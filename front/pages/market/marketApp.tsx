d2bc2d4df9e1ba0e918dbba142a700ab9ae109e8
lhj12ps <lhj12ps@gmail.com>
2022-05-24 16:41:36 +0900

    hancoco push 전 커밋

--- front/pages/market/marketApp.tsx
+++ front/pages/market/marketApp.tsx
@@ -1,17 +1,49 @@
-import React, { FC, useEffect } from "react";
+import React, { FC, useEffect, useState } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import reportWebVitals from "./reportWebVitals";
+import { mintAnimalTokenContract } from "./web3Config";
 
 import Main from "./main";
 
 const MarketApp: FC = () => {
+  const [account, setAccount] = useState<string>("");
+
+  const getAccount = async () => {
+    try {
+      if (window.ethereum) {
+        const accounts = await window.ethereum.request({
+          method: "eth_requestAccounts",
+        });
+
+        setAccount(accounts[0]);
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const getBalanceOf = async () => {
+    const response = await mintAnimalTokenContract.methods
+      .balanceOf(account)
+      .call();
+
+    console.log(response);
+  };
+
+  useEffect(() => {
+    getAccount();
+
+    if (account) {
+      getBalanceOf();
+    }
+  }, [account]);
+
   return (
     <Router>
       <Routes>
-        <Route path="/market/main" element={<Main />} />
+        <Route path="/" element={<Main />} />
       </Routes>
     </Router>
   );
 };
-
 export default MarketApp;
