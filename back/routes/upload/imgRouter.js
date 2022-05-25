abf8e51ef1e75cd0e56d65c5633ac9f365b44848
kimhelp <operat8009@gmail.com>
2022-05-25 14:27:06 +0900

    중간점검

--- back/routes/upload/imgRouter.js
+++ back/routes/upload/imgRouter.js
@@ -1,12 +1,12 @@
 const path = require("path");
-// const express = require("express");
-// const app = express();
-const router = express.Router()
+const express = require("express");
+const app = express();
+// const router = express.Router()
 const imgController = require('./imgController.js')
 const multer = require("multer");
 const PORT = 4001;
 
-router.post('/img',imgController.img)
+app.post('/img',imgController.img)
 
 const storage = multer.diskStorage({
   destination: "./public/img/",
@@ -20,9 +20,11 @@ const upload = multer({
     limits: { fileSize: 1000000 }
   });
   
-  router.post("/upload", upload.single("img"), function(req, res, next) {
+  app.post("/upload", upload.single("img"), function(req, res, next) {
     res.send({
       fileName: req.file.filename
     });
     
   });
+
+  module.exports = router
